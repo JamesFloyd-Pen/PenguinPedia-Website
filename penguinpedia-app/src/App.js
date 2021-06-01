34cafd7840c1d101e7d50c14e98a92bbb7e2e257
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-06-01 17:40:48 -0400

    6/1/Update

--- penguinpedia-app/src/App.js
+++ penguinpedia-app/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import './components/styles/App.css';
 import './components/Penguin_navBar';
+import './components/Penguin_footer';
 import Penguin_navBar from './components/Penguin_navBar';
+import Penguin_footer from './components/Penguin_footer';
 
 function App() {
   return (
     <div>
       <Penguin_navBar/>
-
+      <Penguin_footer/>
     </div>
 
   );
---
+++ penguinpedia-app/src/components/Penguin_footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+//The footer
+class Penguin_footer extends Component {
+    state = {  }
+    render() { 
+        return (
+            <footer> 
+                <div>
+                    <h1>HELLO!</h1>
+                </div>
+            </footer>
+          );
+    }
+}
+ 
+export default Penguin_footer;
