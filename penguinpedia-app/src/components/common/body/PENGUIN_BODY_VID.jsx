8c8f6db9f13e8e243f5791ae33bd410b81643414
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-06-07 15:45:57 -0400

    got the background image to work
    Yay

--- penguinpedia-app/src/components/common/body/PENGUIN_BODY_VID.jsx
+++ penguinpedia-app/src/components/common/body/PENGUIN_BODY_VID.jsx
@@ -1,16 +1,18 @@
 import React, 
 { Component } from 'react';
 
-const backgroundImage = process.env.PUBLIC_URL + '/images/HD_Emperor_Penguin.jpg';
+const backgroundImage = process.env.PUBLIC_URL + '/HD_Emperor_Penguin.jpg';
+//const backgroundImage = process.env.src +'src/components/images/HD_Emperor_Penguin.jpg';
+
 
 class PENGUIN_BODY_VID extends Component {
     state = {  }
     render() { 
         return ( 
-            <div style={{'backgroundImage': `url(${backgroundImage})`}}>
+            <div className="penguin-hero-body" style={{'backgroundImage': `url(${backgroundImage})`}}>
                 <p>A</p>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/lF9qWq3uVcg" title="YouTube video player" 
-                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
 
                 </iframe>
             </div>
