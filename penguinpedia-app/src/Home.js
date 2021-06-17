44fbf82e371a279b75b44d75b221a38698bf98e9
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-06-17 11:31:03 -0400

    Second Update

--- penguinpedia-app/src/Home.js
+++ penguinpedia-app/src/Home.js
@@ -2,18 +2,19 @@ import React from "react";
 import './components/PENGUIN_NAVBAR';
 import './components/PENGUIN_FOOTER';
 import './components/common/body/PENGUIN_BODY_VID';
-import './components/common/body/PENGUIN_BODY_HERO_DAILYPENGUIN'
+import './components/common/body/card/PENGUIN_BODY_HERO_DAILYPENGUIN'
 import PENGUIN_NAVBAR from './components/PENGUIN_NAVBAR';
 import PENGUIN_FOOTER from './components/PENGUIN_FOOTER';
 import PENGUIN_BODY_VID from './components/common/body/PENGUIN_BODY_VID';
-import PENGUIN_BODY_HERO_DAILYPENGUIN from "./components/common/body/PENGUIN_BODY_HERO_DAILYPENGUIN";
+import PENGUIN_BODY_HERO_DAILYPENGUIN from "./components/common/body/card/PENGUIN_BODY_HERO_DAILYPENGUIN";
+//            
 
 function Home(){
     return(
-        <div>
+        <div className="Hero">
             <PENGUIN_NAVBAR />
-            <PENGUIN_BODY_HERO_DAILYPENGUIN />
             <PENGUIN_BODY_VID />
+            <PENGUIN_BODY_HERO_DAILYPENGUIN />
             <PENGUIN_FOOTER />
         </div>
     );
---
+++ penguinpedia-app/src/components/common/body/card/PENGUIN_BODY_HERO_DAILYPENGUIN.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Card, Button, CardGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CARD_IMAGE_STYLE = {
+    height: '20%',
+    width: '20%',
+    display: 'block',
+    margin: 'auto'
+};
+
+class PENGUIN_BODY_HERO_DAILYPENGUIN extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <CardGroup>
+            <Card style={{ width: '18rem' }} >
+                <Card.Img variant="top" src="/images/Penguin-High-Quality-PNG.png" style={CARD_IMAGE_STYLE}/>
+                <Card.Body>
+                <Card.Text>
+                    The Emperor Penguin is the largest penguins in the world, and inhabit in antarctica. 
+                </Card.Text>
+                <Button variant="primary">Learn More</Button>
+                </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="/images/Penguin-High-Quality-PNG.png" style={CARD_IMAGE_STYLE}/>
+                <Card.Body>
+                <Card.Text>
+                    The Fairy Penguin is the smallest penguin in the world. 
+                </Card.Text>
+                <Button variant="primary">Learn More</Button>
+                </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="/images/Penguin-High-Quality-PNG.png" style={CARD_IMAGE_STYLE}/>
+                <Card.Body>
+                <Card.Text>
+                    African Penguin 
+                </Card.Text>
+                <Button variant="primary">Learn More</Button>
+                </Card.Body>
+          </Card>
+          </CardGroup>
+         );
+    }
+}
+ 
+export default PENGUIN_BODY_HERO_DAILYPENGUIN;
