42d9aa1814aa2ec5a07767ece2b75ecf8f6b31fc
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-06-17 09:21:57 -0400

    Morning Update 6/17
    Added a sample card. Temporary right now

--- penguinpedia-app/src/Home.js
+++ penguinpedia-app/src/Home.js
@@ -2,15 +2,17 @@ import React from "react";
 import './components/PENGUIN_NAVBAR';
 import './components/PENGUIN_FOOTER';
 import './components/common/body/PENGUIN_BODY_VID';
+import './components/common/body/PENGUIN_BODY_HERO_DAILYPENGUIN'
 import PENGUIN_NAVBAR from './components/PENGUIN_NAVBAR';
 import PENGUIN_FOOTER from './components/PENGUIN_FOOTER';
 import PENGUIN_BODY_VID from './components/common/body/PENGUIN_BODY_VID';
-
+import PENGUIN_BODY_HERO_DAILYPENGUIN from "./components/common/body/PENGUIN_BODY_HERO_DAILYPENGUIN";
 
 function Home(){
     return(
         <div>
             <PENGUIN_NAVBAR />
+            <PENGUIN_BODY_HERO_DAILYPENGUIN />
             <PENGUIN_BODY_VID />
             <PENGUIN_FOOTER />
         </div>
--- penguinpedia-app/src/components/common/body/PENGUIN_BODY_VID.jsx
+++ penguinpedia-app/src/components/common/body/PENGUIN_BODY_VID.jsx
@@ -3,7 +3,6 @@ import React,
 
 const backgroundImage = process.env.PUBLIC_URL + '/images/HD_Emperor_Penguin.jpg';
 //const backgroundImage = process.env.src +'src/components/images/HD_Emperor_Penguin.jpg';
-//
 
 
 class PENGUIN_BODY_VID extends Component {
@@ -11,10 +10,8 @@ class PENGUIN_BODY_VID extends Component {
     render() { 
         return ( 
             <div className="penguin-hero-body" style={{'backgroundImage': `url(${backgroundImage})`}}>
-                <p>A</p>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/lF9qWq3uVcg" title="YouTube video player" 
                 frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
-
                 </iframe>
             </div>
 
