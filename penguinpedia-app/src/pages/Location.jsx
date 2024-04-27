a23284c409f9a02a5fef1343d3da75b82f91ba29
JamesFloyd-Pen <Commanderpip@gmail.com>
2024-04-27 17:08:25 -0400

    Outline of the Project
    JSX files

---
+++ penguinpedia-app/src/pages/Location.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Location = () => {
+    return (
+        <div>
+            <h1>Penguin Species Across the Globe</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default Location;
---
+++ penguinpedia-app/src/pages/Penguins.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Penguins() {
+    return (
+        <div>
+            <h1>Penguins Page</h1>
+            <p>Welcome to the Penguins page!</p>
+        </div>
+    );
+}
+
+export default Penguins;
