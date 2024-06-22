4a8b29ad1e11f6fd4fa3ac970024da80492db512
JamesFloyd-Pen <Commanderpip@gmail.com>
2024-06-22 15:58:44 -0400

    About page demo
    About page demo

--- penguinpedia-app/src/pages/About.js
+++ penguinpedia-app/src/pages/About.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../components/styles/About.css';
 import penguinImage from '../components/img/Emperor-Penguin-March.jpg';
+import penguinPC from '../components/img/penguin-on-pc.jpg';
+import JPenImage from '../components/img/J-Pen-Avatar.png';
 import { Container, Row, Col } from 'react-bootstrap';
-
-
 
 function About() {
     return (
@@ -19,25 +19,25 @@ function About() {
                 </Col>
                 <Col md={6}>
                     <h2 className="about-heading">What is PenguinPedia?</h2>
-                    <p className="about-text">PenguinPedia provides information of all 18 penguin species across the globe. It is a great resource for people who are interested in learning more about these fascinating birds. Alongside this we aim to bring awareness of the dangers the penguins face in modern times.</p>
+                    <p className="about-text">PenguinPedia provides information on all 18 penguin species worldwide. It is an excellent resource for people who are interested in learning more about these fascinating birds. We also aim to raise awareness of the dangers penguins face in modern times.</p>
                 </Col>
             </Row>
             <Row>
                 <Col md={6}>
                     <h2 className="about-heading">Goal of PenguinPedia</h2>
-                    <p className="about-text">The Goal of PenguinPedia is to inform people of these awesome birds and the challenges they face. As global warning continues to effect the earth, penguins increasing face challenges . One of the biggest I learn is from news to even discussions from my family on what tragedies occured in the south pole. </p>
+                    <p className="about-text">The Goal of PenguinPedia is to inform people about these awesome birds and the challenges they face. As global warming continues to affect the earth, penguins are facing increasing challenges. One of the biggest I learned is from news and even discussions with my family on what tragedies occurred at the South Pole.</p>
                 </Col>
                 <Col md={6}>
-                    <p>Insert IMAGE HERE</p>
+                    <img className="about-penguin-image" src={penguinPC} alt="Penguin on PC" />
                 </Col>
             </Row>
             <Row>
                 <Col md={6}>
                     <h2 className="about-heading">About the Author</h2>
-                    <p className="about-text">Hello, my name is James Floyd. I grew up as a penguin enthusiastic during the 2000  penguin craze of the  time. I learned much of about these fascinating birds when I watched March of the Penguins, Madcessanger, and read numurous books like Mr Hopper's Penguins. </p>
+                    <p className="about-text">Hello, my name is James Floyd. I grew up as a penguin enthusiast during the 2000  penguin craze. I learned much about these fascinating birds when I watched March of the Penguins and Madagascar and read numerous books like Mr Hopper's Penguins. </p>
                 </Col>
                 <Col md={6}>
-                    <p>Insert IMAGE HERE</p>
+                    <img className="about-penguin-image" src={JPenImage} alt="James Floyd Avatar 2" />
                 </Col>
             </Row>
             <Row>
--- penguinpedia-app/src/pages/Emperor_Penguin.js
+++ penguinpedia-app/src/pages/Emperor_Penguin.js
@@ -2,10 +2,50 @@ import React from 'react';
 import '../components/styles/Penguins.css';
 import penguinImage from '../components/img/emperorpenguin.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 function EmperorPenguin() {
     return (
-        <div className="info-container center-content">
+        <Container>
+            <Row>
+                <Col className="info-section">
+                    <h2>Overview</h2>
+                    <div className="image-box space-below">
+                        <img src={penguinImage} alt="Emperor Penguin" />
+                    </div>
+                </Col>
+                <Col className="info-penguin-box" xs={12} md={3}>
+                    <h2>Penguin Facts</h2>
+                    <ul>
+                        <li><strong>Name:</strong> Emperor Penguin </li>
+                        <li><strong>Scientific Name:</strong> Aptenodytes forsteri</li>
+                        <li><strong>Family:</strong> Spheniscidae</li>
+                        <li><strong>Height:</strong> Up to 130 cm</li>
+                        <li><strong>Weight:</strong> 22 to 45 kg</li>
+                        <li><strong>Lifespan:</strong> 15 to 20 years</li>
+                        <li><strong>Diet:</strong> Mainly fish and squid</li>
+                        <li><strong>Population:</strong> Around 595,000</li>
+                        <li><strong>Habitat:</strong> Sea ice in Antarctica</li>
+                    </ul>
+                </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+                <Col>
+                    <p className="overview-text">
+                        The Emperor Penguin is the largest and heaviest of all penguin species. They are native to Antarctica and are known for their unique breeding habits. Emperor Penguins are the only penguins that breed during the harsh Antarctic winter. They are also the deepest diving and the most aquatic of all penguins.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default EmperorPenguin;
+
+/*
+
+<div className="info-container center-content">
             <div className="row">
                 <div className="info-section col">
                     <h2>Overview</h2>
@@ -32,7 +72,9 @@ function EmperorPenguin() {
                 </div>
             </div>
         </div>
-    );
-}
 
-export default EmperorPenguin;
+
+
+
+
+*/
