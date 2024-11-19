510928b0f19708b9fe4dd5a4cebb4cec2fa3e2d0
James Floyd <Commanderpip@gmail.com>
2024-11-19 18:07:18 -0500

    Footer Update
    Link update for the about footer link to the about is able to work.

--- penguinpedia-app/src/components/Penguin_footer.jsx
+++ penguinpedia-app/src/components/Penguin_footer.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 //The footer
 class PENGUIN_FOOTER extends Component {
@@ -10,7 +12,7 @@ class PENGUIN_FOOTER extends Component {
                 <div className="Row-Foot-Container">
                     <h1>PenguinPedia</h1>
                         <ul>
-                            <li>About</li>
+                            <li><Link to="/about">About</Link></li>
                             <li><a href="https://www.penguinsinternational.org/donate/">Donate</a></li>
                             <li><a href="https://github.com/JamesFloyd-Pen">Github</a></li>
                         </ul>
